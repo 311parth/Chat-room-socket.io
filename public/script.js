26cb4a4502d1955724ea7f6953504fa193bd2748
311parth <parthghoghari311@gmail.com>
2023-01-23 19:11:43 +0530

    redesigned backend and added basic rooms

--- public/script.js
+++ public/script.js
@@ -1,16 +1,21 @@
 
 const socket = io();
-console.log("jh")
-console.log(socket)
+// console.log("jh")
+// console.log(socket)
 socket.on("connect",()=>{
-    console.log(socket.id)
-    console.log(socket.connected)
+    console.log(socket)
+    // console.log(socket.id)
+    // console.log(socket.connected)
 })
 
+var inputUsername = document.getElementById("input-username")
 var username ;
-if(window.location.pathname==="/"){
-    username = prompt("Enter your name");
-}
+inputUsername && inputUsername.addEventListener("change",()=>{
+    username = inputUsername.value;
+    // console.log("us :",inputUsername.value,typeof(username))
+})
+
+
 
 
 const uid = window.location.pathname.toString().substr(-6,6);
@@ -25,11 +30,15 @@ const newJoinedMsg = (newJoinedUsername) => `<div class="newJoinedMsg">
 </div>
 </div>`;
 socket.on("newJoined",(args)=>{
-    msgContainer.innerHTML+=newJoinedMsg(args)
-    console.log("new user joined",args)
+    if(msgContainer) msgContainer.innerHTML+=newJoinedMsg(args)
+    // console.log("new user joined",args)
 })
 
-
+// if(window.location.pathname)
+if(window.location.pathname!=="/"){
+    socket.emit("join-room",uid)
+}
+ 
 const copyBtn = document.querySelector(".copy-btn");
 if(copyBtn){
     copyBtn.addEventListener("click",()=>{
@@ -37,34 +46,38 @@ if(copyBtn){
         copyBtn.innerText = "copied"
     })
 }
+const newRoomBtn = document.getElementById("new-room-btn");
+if(newRoomBtn){
+    newRoomBtn.addEventListener("click",()=>{
+        if(inputUsername.value===""){
+            alert("enter username")
+            return;
+        }
+        socket.emit("getNewRoom","");//to create new room 
+        socket.on("redirectionToNewRoom",(args)=>{ //it will return room url for new room
+            // console.log(args)
+            window.location.pathname=args;   
+        })
+    })
+}
+
+
 
 const joinRoomBtn = document.getElementById("join-room-btn");
 if(joinRoomBtn){
     joinRoomBtn.addEventListener("click",()=>{
         const inputedId = document.getElementById("input-join-id").value;
-        console.log(typeof(inputedId))
-
-        var body = {
-            username : username,
-            roomid: inputedId
-        }
-        console.log(JSON.stringify(body))
-            fetch("redirect/chatroom",{
-                method:"POST",
-                headers:{
-                    "Content-Type": "application/json"
-                },
-                body : JSON.stringify(body)
-            })
-            .then((res)=>res.json())
-            .then((data)=>{
-                // console.log(data)
-                if(data.isok){
-                    window.location = `http://localhost:3000/room/id/${inputedId}`
-                }else{
-                    alert("Enter valid room id")
-                }
-            })
+        socket.emit("joinRoomReq",{reqid : inputedId,username : username})
+        socket.on("joinRoomReqResponse",(resRoomReq)=>{
+            // console.log(resRoomReq)
+            if(resRoomReq.success){
+                window.location = `http://localhost:3000/room/id/${resRoomReq.roomid}`
+            }
+        })
     })
 }
+
+// socket.on("redirectionToNewRoom",(args)=>{
+//     console.log(args)
+// })
 
--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var currentRunningId = ["1"];
     //     console.log("no")
     // }
     // console.log(currentRunningId)
-    
+
 app.use(express.static("public"))
 app.use(express.json())
 app.get("/",(req,res)=>{
@@ -33,7 +33,6 @@ app.get("/room",(req,res)=>{
 
 const getUid = ()=>{
     const uid = randomId(6);
-    
     if(currentRunningId.includes(uid)){
         getUid();
     }else
@@ -43,13 +42,16 @@ const getUid = ()=>{
     }
 }
 // console.log(getUid())
-app.get("/newroom",(req,res)=>{
-    const uid= getUid();
-    // console.log(uid)
-    // console.log(currentRunningId)
 
-    res.redirect(`/room/id/${uid}`)
-})
+// app.get("/newroom",(req,res)=>{
+//     const uid= getUid();
+//     // console.log(uid)
+//     // console.log(currentRunningId)
+//     io.on("connection", (socket) => {
+//         socket.join(uid);
+//     });
+//     res.redirect(`/room/id/${uid}`)
+// })
 
 
 app.get("/room/id/:id",(req,res)=>{
@@ -60,23 +62,48 @@ app.get("/room/id/:id",(req,res)=>{
     }
 })
 
-app.post("/redirect/chatroom",(req,res)=>{
-    // console.log(currentRunningId)
-    const newJoinedUsername = req.body.username;
-    const roomId = req.body.roomid;
-    if(currentRunningId.includes(roomId)){
-        io.on("connection",(socket)=>{
-            // console.log("connected",socket.id)
-            socket.broadcast.emit("newJoined",newJoinedUsername)
-        })
-        res.json({"isok":1})
-        
-    }else{
-        res.json({"isok":0});
-    }
-})
+// app.post("/redirect/chatroom",(req,res)=>{
+//     // console.log(currentRunningId)
+//     var newJoinedUsername = req.body.username;
+//     const roomId = req.body.roomid;
+//     if(currentRunningId.includes(roomId)){
+//         io.on("connection",(socket)=>{
+//             // console.log("connected",socket.id)
+//             socket.join(roomId);
+//             // socket.broadcast.emit("newJoined",newJoinedUsername)
+//             io.to(roomId).timeout(1000).emit("newJoined",newJoinedUsername);
+//             console.log("emited",newJoinedUsername)
+//         })
+//         res.json({"isok":1})
+//     }else{
+//         res.json({"isok":0});
+//     }
+// })
 const io = require("socket.io")(httpServer)
 
 io.on("connection",(socket)=>{
+    socket.on("join-room",id=>{
+        if(currentRunningId.includes(id)){
+            socket.join(id);
+        }
+    })
     console.log("connected",socket.id)
+    socket.on("getNewRoom",()=>{
+        const uid= getUid();
+        socket.join(uid);
+        socket.emit("redirectionToNewRoom",`/room/id/${uid}`)
+    })
+    socket.on("joinRoomReq",(req)=>{
+        // console.log("join room req got ",req)
+        if(currentRunningId.includes(req.reqid)){
+            socket.join(req.reqid)
+            // console.log("to",io.sockets.adapter.rooms.has(req.reqid))
+            io.sockets.in(req.reqid).emit('newJoined', req.username);
+
+            socket.emit("joinRoomReqResponse",{success:1,roomid:req.reqid})
+        }else{
+            socket.emit("joinRoomReqResponse",{success:0,roomid:req.reqid})
+        }
+    })
+
 })
